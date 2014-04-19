f567f527b7057bed675119d7a2f850e6c048421b
Simon Chaffetz <simis1000@gmail.com>
2014-04-19 11:34:57 -0700

    Initial Commit

---
+++ tip_calulator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tip_calulator
+//
+//  Created by Simon Chaffetz on 4/19/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ tip_calulatorTests/tip_calulatorTests.m
@@ -0,0 +1,34 @@
+//
+//  tip_calulatorTests.m
+//  tip_calulatorTests
+//
+//  Created by Simon Chaffetz on 4/19/14.
+//
+//
+
+#import <XCTest/XCTest.h>
+
+@interface tip_calulatorTests : XCTestCase
+
+@end
+
+@implementation tip_calulatorTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
